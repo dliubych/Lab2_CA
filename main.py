04ffcd00ddb7c0182ea791b7e008db68a5d42560
Maxim <maksim.bondarchuk@gmail.com>
2015-04-26 23:36:10 +0300

    Without server

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from bottle import *
 import bottle
 
+sent_parts = []
+free_parts = []
+received_parts = []
+all_palindromes = []
+n = 4
+
 
 @bottle.get('/scripts/:filename#.*#')
 def send_static(filename):
@@ -14,8 +20,23 @@ def index():
 
 @get('/workerData')
 def worker_get():
-    # return "abc"
-    return {'n': 4, 'text': 'abba bbbb aaaa  aaaa casd', 'worker_number': 0}
+    for i in range(1, 101):
+        if i not in sent_parts and i not in received_parts:
+            sent_parts.append(i)
+            worker_text = text[(len(text) - n) * (i - 1) / 100: (len(text) - n) * i / 100 + n]
+            # print('worker #%d working on \'%s\'' % (i, worker_text))
+            return {'n': n, 'text': worker_text, 'worker_number': i}
+
+    for i in range(1, 101):
+        if i not in received_parts:
+            # sent_parts.append(i)
+            worker_text = text[(len(text) - n) * (i - 1) / 100: (len(text) - n) * i / 100 + n]
+            # print('worker #%d working on \'%s\'' % (i, worker_text))
+            return {'n': n, 'text': worker_text, 'worker_number': i}
+
+    for i in all_palindromes:
+        print(i)
+    return {'n': -1, 'text': '', 'worker_number': -1}   # Message that worker needs to stop
 
 
 @post('/workerData')
@@ -23,11 +44,27 @@ def worker_post():
     worker_number = request.forms.get('worker_number')
     palindromes = request.forms.get('palindromes')
 
+    if worker_number not in received_parts:
+        received_parts.append(int(worker_number))
+        if int(worker_number) in sent_parts:
+            sent_parts.remove(int(worker_number))
+
     # Just debugging
-    print('I`m worker #%s and I found next palindromes:' % worker_number)
+    # print('I`m worker #%s and I found next palindromes:' % worker_number)
+    print('Part #%s finished' % worker_number)
+    was_something = False
     for i in palindromes.split(','):
-        print(i)
-    print()
+        if '\n' not in i and i not in all_palindromes:
+            all_palindromes.append(i)
+            # was_something = True
+            # print(i)
+    # if not was_something:
+    #     print('<none>')
+    # print('Sent parts:     ' + str(sent_parts))
+    # print('Received parts: ' + str(received_parts))
+    # print('')
 
 
+file_with_text = open('text.txt', 'r')
+text = file_with_text.read()
 run(host='localhost', port=8080, debug=True)
--- scripts/worker.js
+++ scripts/worker.js
@@ -16,22 +16,42 @@ function getPalindromes(text, n) {
 }
 
 
-var text, n, workerNumber;
-xmlhttpGET = new XMLHttpRequest();
-xmlhttpGET.onreadystatechange = function () {
-    if (xmlhttpGET.readyState == 4 && xmlhttpGET.status == 200) {
-        var whatWeGot = JSON.parse(xmlhttpGET.responseText);
-        text = whatWeGot['text'];
-        n = whatWeGot['n'];
-        workerNumber = whatWeGot['worker_number'];
+var text, n = 100, workerNumber;
+//var xmlhttpGET, xmlhttpPOST;
 
+while (n != -1) {
+    var xmlhttpGET = new XMLHttpRequest();
+//xmlhttpGET.onreadystatechange = function () {
+//    if (xmlhttpGET.readyState == 4 && xmlhttpGET.status == 200) {
+//        var whatWeGot = JSON.parse(xmlhttpGET.responseText);
+//        text = whatWeGot['text'];
+//        n = whatWeGot['n'];
+//        workerNumber = whatWeGot['worker_number'];
+//
+//        var palindromes = getPalindromes(text, n);
+//
+//        xmlhttpPOST = new XMLHttpRequest();
+//        xmlhttpPOST.open("POST", "/workerData", true);
+//        xmlhttpPOST.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//        xmlhttpPOST.send("worker_number=" + workerNumber + "&palindromes=" + palindromes);
+//    }
+//};
+    xmlhttpGET.open("GET", "/workerData", false);
+    xmlhttpGET.send();
+
+    var whatWeGot = JSON.parse(xmlhttpGET.responseText);
+    text = whatWeGot['text'];
+    n = whatWeGot['n'];
+    workerNumber = whatWeGot['worker_number'];
+
+    if (n != -1) {
         var palindromes = getPalindromes(text, n);
 
-        xmlhttpPOST = new XMLHttpRequest();
+        var xmlhttpPOST = new XMLHttpRequest();
         xmlhttpPOST.open("POST", "/workerData", true);
         xmlhttpPOST.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xmlhttpPOST.send("worker_number=" + workerNumber + "&palindromes=" + palindromes);
+
+        //setTimeout(function () {}, 2000);
     }
-};
-xmlhttpGET.open("GET", "/workerData", true);
-xmlhttpGET.send();
+}
